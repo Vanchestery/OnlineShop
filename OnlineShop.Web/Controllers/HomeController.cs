894efd39e9f88c1d4427de5d72e976f83fdffe6e
Иван <ivan-kanninen@mail.ru>
2026-05-21 13:04:28 +0300

    feat(web): catalog with search, product details, layout, Serilog setup

--- OnlineShop.Web/Controllers/HomeController.cs
+++ OnlineShop.Web/Controllers/HomeController.cs
@@ -1,19 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Core.Interfaces;
 using OnlineShop.Web.Models;
+using OnlineShop.Web.ViewModels.Catalog;
 
 namespace OnlineShop.Web.Controllers;
 
 public class HomeController : Controller
 {
+    private const int FeaturedCount = 4;
+
+    private readonly IProductService _products;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IProductService products, ILogger<HomeController> logger)
     {
+        _products = products;
         _logger = logger;
     }
 
-    public IActionResult Index() => View();
+    public async Task<IActionResult> Index(CancellationToken ct)
+    {
+        var all = await _products.GetAllAsync(includeUnavailable: false, ct);
+        var featured = all
+            .Take(FeaturedCount)
+            .Select(ProductCardViewModel.FromDto)
+            .ToList();
+        return View(featured);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
